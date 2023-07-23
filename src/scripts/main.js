9275319a591451681286ee68facc7ec56cb03100
lidice <lidicedupuit@hotmail.com>
2023-07-22 21:23:59 -0300

    commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('form-cadastro').addEventListener('submit', function(){
+        let nomeCompleto = document.getElementById('nome-completo');
+        let telefoneCelular = document.getElementById('telefone-celular');
+    })
+})
